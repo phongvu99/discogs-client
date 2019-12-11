ed32eb0b139406fbae80b3c89f8f919f18dd0697
Tieuphi99 <tieuphi99@gmail.com>
2019-12-11 22:13:55 +0700

    WIP, need debug, BuyMusicActivity

--- src/main/java/com/naughtybitch/discogsclient/BuyMusicActivity.java
+++ src/main/java/com/naughtybitch/discogsclient/BuyMusicActivity.java
@@ -2,6 +2,7 @@ package com.naughtybitch.discogsclient;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -15,35 +16,14 @@ public class BuyMusicActivity extends AppCompatActivity implements View.OnClickL
         CartFragment.OnFragmentInteractionListener,
         DWFragment.OnFragmentInteractionListener,
         PurchasesFragment.OnFragmentInteractionListener,
-        OIMFragment.OnFragmentInteractionListener {
+        OIMFragment.OnFragmentInteractionListener,
+        BuyMusicFragment.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy_music);
         buttonOnClickListener();
-
-        // Check that the activity is using the layout version with
-        // the fragment_container FrameLayout
-        if (findViewById(R.id.fragment_container) != null) {
-
-            // However, if we're being restored from a previous state,
-            // then we don't need to do anything and should return or else
-            // we could end up with overlapping fragments.
-            if (savedInstanceState != null) {
-                return;
-            }
-        }
-
-        // Create a new Fragment to be placed in the activity layout
-        HomeFragment firstFragment = HomeFragment.newInstance();
-
-        // In case this activity was started with special instructions from an
-        // Intent, pass the Intent's extras to the fragment as arguments
-        firstFragment.setArguments(getIntent().getExtras());
-
-        // Add the fragment to the 'fragment_container' FrameLayout
-        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, firstFragment).commit();
     }
 
     @Override
