7aff7f2f90ee541502fcb6f43aee038897ab7167
tieuphi99 <tieuphi99@gmail.com>
2019-12-17 23:36:16 +0700

    Something wrong with AppIntro, crash on start, need help from mah backman Phong Vu

--- src/main/java/com/naughtybitch/discogsclient/welcome/WelcomeActivity.java
+++ src/main/java/com/naughtybitch/discogsclient/welcome/WelcomeActivity.java
@@ -1,203 +1,42 @@
 package com.naughtybitch.discogsclient.welcome;
 
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
-import android.text.Html;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.os.PersistableBundle;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.PagerAdapter;
-import androidx.viewpager.widget.ViewPager;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
-import com.naughtybitch.discogsclient.MainActivity;
+import com.github.paolorotolo.appintro.AppIntro;
+import com.naughtybitch.discogsclient.GetStartedActivity;
 import com.naughtybitch.discogsclient.R;
 
-public class WelcomeActivity extends AppCompatActivity {
-
-    private ViewPager pager;
-    private MyViewPagerAdapter adapter;
-    private LinearLayout dotsLayout;
-    private TextView[] dots;
-    private int[] layouts;
-    private Button btnSkip, btnNext;
-    ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
-
-        @Override
-        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-        }
-
-        @Override
-        public void onPageSelected(int position) {
-            addBottomDots(position);
-
-            // changing the next button text 'NEXT' / 'GOT IT'
-            if (position == layouts.length - 1) {
-                // last page. make button text to GOT IT
-                btnNext.setText(getString(R.string.start));
-                btnSkip.setVisibility(View.GONE);
-            } else {
-                // still pages are left
-                btnNext.setText(getString(R.string.next));
-                btnSkip.setVisibility(View.VISIBLE);
-            }
-        }
-
-        @Override
-        public void onPageScrollStateChanged(int state) {
-
-        }
-    };
-    private PrefManager prefManager;
+public class WelcomeActivity extends AppIntro {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        addSlide(SampleSlide.newInstance(R.layout.welcome_slide_1));
+        addSlide(SampleSlide.newInstance(R.layout.welcome_slide_2));
+        addSlide(SampleSlide.newInstance(R.layout.welcome_slide_3));
+        addSlide(SampleSlide.newInstance(R.layout.welcome_slide_4));
+        setFadeAnimation();
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        // Checking for the first time launch - before calling setContentView()
-        prefManager = new PrefManager(this);
-        if (!prefManager.isFirstTimeLaunch()) {
-            launchHomeScreen();
-            finish();
-        }
-
-        // Making notification bar transparent
-        if (Build.VERSION.SDK_INT >= 21) {
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        }
-
-        setContentView(R.layout.activity_welcome);
-
-        pager = findViewById(R.id.view_pager);
-        dotsLayout = findViewById(R.id.layoutDots);
-        btnSkip = findViewById(R.id.btn_skip);
-        btnNext = findViewById(R.id.btn_next);
-
-        // Layouts of all welcome sliders
-        // add few more layouts if you want
-        layouts = new int[]{
-                R.layout.welcome_slide_1,
-                R.layout.welcome_slide_2,
-                R.layout.welcome_slide_3,
-                R.layout.welcome_slide_4};
-
-        // Adding bottom dots
-        addBottomDots(0);
-
-        // Making notification bar transparent
-        changeStatusBarColor();
-
-        adapter = new MyViewPagerAdapter();
-        pager.setAdapter(adapter);
-        pager.addOnPageChangeListener(viewPagerPageChangeListener);
-
-        btnSkip.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                launchHomeScreen();
-            }
-        });
-
-        btnNext.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Checking for last page
-                // if last page -> home screen will be launched
-                int current = getItem(1);
-                if (current < layouts.length) {
-                    // Move to next screen
-                    pager.setCurrentItem(current);
-                } else {
-                    launchHomeScreen();
-                }
-            }
-        });
-    }
-
-    private void addBottomDots(int currentPage) {
-
-        int[] colorsActive = getResources().getIntArray(R.array.array_dot_active);
-        int[] colorsInactive = getResources().getIntArray(R.array.array_dot_inactive);
-        dots = new TextView[layouts.length];
-
-        dotsLayout.removeAllViews();
-        for (int i = 0; i < dots.length; i++) {
-            try {
-                dots[i] = new TextView(this);
-                dots[i].setText(Html.fromHtml("&#8226;"));
-                dots[i].setTextSize(35);
-                dots[i].setTextColor(colorsInactive[currentPage]);
-                dotsLayout.addView(dots[i]);
-            } catch (ArrayIndexOutOfBoundsException e) {
-                Log.i("sucker", "sucker" + dots[i]);
-                e.printStackTrace();
-            }
-        }
-        if (dots.length > 0)
-            dots[currentPage].setTextColor(colorsActive[currentPage]);
-    }
-
-    private void changeStatusBarColor() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Window window = getWindow();
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(Color.TRANSPARENT);
-        }
-    }
-
-    private int getItem(int i) {
-        return (pager.getCurrentItem() + i);
-    }
-
-    private void launchHomeScreen() {
-        prefManager.setFirstTimeLaunch(false);
-        startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+    public void onSkipPressed(Fragment currentFragment) {
+        super.onSkipPressed(currentFragment);
+//        Intent intent = new Intent(this, GetStartedActivity.class);
         finish();
     }
 
-    // ViewPager adapter
-    public class MyViewPagerAdapter extends PagerAdapter {
-        private LayoutInflater layoutInflater;
+    @Override
+    public void onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
+        finish();
+    }
 
-        public MyViewPagerAdapter() {
-        }
-
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
-            View view = layoutInflater.inflate(layouts[position], container, false);
-            container.addView(view);
-
-            return view;
-        }
-
-        @Override
-        public int getCount() {
-            return layouts.length;
-        }
-
-        @Override
-        public boolean isViewFromObject(View view, Object obj) {
-            return view == obj;
-        }
-
-
-        @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            View view = (View) object;
-            container.removeView(view);
-        }
+    @Override
+    public void onSlideChanged(@Nullable Fragment oldFragment, @Nullable Fragment newFragment) {
+        super.onSlideChanged(oldFragment, newFragment);
     }
 }
