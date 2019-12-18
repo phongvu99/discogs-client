4429a0d910f13ee200191ea33603d0c6dc89c6ab
Tieuphi99 <tieuphi99@gmail.com>
2019-12-18 11:10:24 +0700

    Welcome

--- src/main/java/com/naughtybitch/discogsclient/welcome/WelcomeActivity.java
+++ src/main/java/com/naughtybitch/discogsclient/welcome/WelcomeActivity.java
@@ -2,7 +2,6 @@ package com.naughtybitch.discogsclient.welcome;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -13,8 +12,8 @@ import com.naughtybitch.discogsclient.R;
 
 public class WelcomeActivity extends AppIntro {
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         addSlide(SampleSlide.newInstance(R.layout.welcome_slide_1));
         addSlide(SampleSlide.newInstance(R.layout.welcome_slide_2));
         addSlide(SampleSlide.newInstance(R.layout.welcome_slide_3));
@@ -25,7 +24,8 @@ public class WelcomeActivity extends AppIntro {
     @Override
     public void onSkipPressed(Fragment currentFragment) {
         super.onSkipPressed(currentFragment);
-//        Intent intent = new Intent(this, GetStartedActivity.class);
+        Intent intent = new Intent(this, GetStartedActivity.class);
+        startActivity(intent);
         finish();
     }
 
