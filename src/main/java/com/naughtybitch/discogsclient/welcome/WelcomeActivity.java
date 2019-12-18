2e0479c3dfca74581c2838ccae3534a3e290efb5
Tieuphi99 <tieuphi99@gmail.com>
2019-12-18 14:43:55 +0700

    Welcome Final Version

--- src/main/java/com/naughtybitch/discogsclient/welcome/WelcomeActivity.java
+++ src/main/java/com/naughtybitch/discogsclient/welcome/WelcomeActivity.java
@@ -1,6 +1,8 @@
 package com.naughtybitch.discogsclient.welcome;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -11,6 +13,9 @@ import com.naughtybitch.discogsclient.GetStartedActivity;
 import com.naughtybitch.discogsclient.R;
 
 public class WelcomeActivity extends AppIntro {
+
+    SharedPreferences sp;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,11 +24,22 @@ public class WelcomeActivity extends AppIntro {
         addSlide(SampleSlide.newInstance(R.layout.welcome_slide_3));
         addSlide(SampleSlide.newInstance(R.layout.welcome_slide_4));
         setFadeAnimation();
+
+        /* In your onCreate method */
+        sp = getSharedPreferences("MyPref", Context.MODE_PRIVATE);
+        if (sp.getBoolean("first", false)) {
+            Intent intent = new Intent(this, GetStartedActivity.class); // Call the AppIntro java class
+            startActivity(intent);
+            finish();
+        }
     }
 
     @Override
     public void onSkipPressed(Fragment currentFragment) {
         super.onSkipPressed(currentFragment);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putBoolean("first", true);
+        editor.apply();
         Intent intent = new Intent(this, GetStartedActivity.class);
         startActivity(intent);
         finish();
@@ -32,6 +48,11 @@ public class WelcomeActivity extends AppIntro {
     @Override
     public void onDonePressed(Fragment currentFragment) {
         super.onDonePressed(currentFragment);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putBoolean("first", true);
+        editor.apply();
+        Intent intent = new Intent(this, GetStartedActivity.class);
+        startActivity(intent);
         finish();
     }
 
